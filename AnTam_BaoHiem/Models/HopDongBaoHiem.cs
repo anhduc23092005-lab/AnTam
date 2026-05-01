d7f1852be4cc74bdb866fdbcf614412f27a93387
duchuyuwu358-rgb <duchuyuwu358@gmail.com>
2026-05-01 22:56:55 +0700

    Add files via upload
    File trường thông tin Quản Lý Hợp Đồng Mua Bảo Hiểm

---
+++ AnTam_BaoHiem/Models/HopDongBaoHiem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FormQuanLyHopDongMuaBaoHiem
+{
+    public class HopDongBaoHiem
+    {
+        public string MaHD { get; set; }
+        public string MaKH { get; set; }
+
+        // Dữ liệu lấy thêm từ bảng KhachHang
+        public string TenCongTy { get; set; }
+        public string MaCongTy { get; set; }
+
+        public string MaGoi { get; set; }
+        public DateTime? NgayKy { get; set; }
+        public DateTime? NgayHetHan { get; set; }
+        public decimal? TongTien { get; set; }
+        public string TrangThai { get; set; }
+    }
+}
