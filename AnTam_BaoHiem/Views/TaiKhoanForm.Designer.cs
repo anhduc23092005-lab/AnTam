304ac56241635991fc94d6ef11a3cb3513ce100f
2311060996-eng <2311060996@hunre.edu.vn>
2026-05-02 08:24:59 +0700

    update

--- AnTam_BaoHiem/Views/TaiKhoanForm.Designer.cs
+++ AnTam_BaoHiem/Views/TaiKhoanForm.Designer.cs
@@ -1,3 +1,4 @@
+// Views/TaiKhoanForm.Designer.cs
 namespace AnTam_BaoHiem.Views
 {
     partial class TaiKhoanForm
@@ -11,9 +12,8 @@ namespace AnTam_BaoHiem.Views
 
         protected override void Dispose(bool disposing)
         {
-            if (disposing && components != null)
+            if (disposing && (components != null))
                 components.Dispose();
-
             base.Dispose(disposing);
         }
 
@@ -62,7 +62,7 @@ namespace AnTam_BaoHiem.Views
             this.dgv.Name = "dgv";
             this.dgv.RowHeadersVisible = false;
             this.dgv.RowHeadersWidth = 51;
-            this.dgv.Size = new System.Drawing.Size(971, 250);
+            this.dgv.Size = new System.Drawing.Size(980, 250);
             this.dgv.TabIndex = 0;
             this.dgv.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
             this.dgv.ThemeStyle.AlternatingRowsStyle.Font = null;
@@ -92,78 +92,78 @@ namespace AnTam_BaoHiem.Views
             this.txtUser.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.txtUser.DefaultText = "";
             this.txtUser.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.txtUser.Location = new System.Drawing.Point(20, 269);
+            this.txtUser.Location = new System.Drawing.Point(20, 270);
             this.txtUser.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtUser.Name = "txtUser";
             this.txtUser.PlaceholderText = "Username";
             this.txtUser.SelectedText = "";
-            this.txtUser.Size = new System.Drawing.Size(199, 48);
-            this.txtUser.TabIndex = 0;
+            this.txtUser.Size = new System.Drawing.Size(200, 40);
+            this.txtUser.TabIndex = 1;
             // 
             // txtPass
             // 
             this.txtPass.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.txtPass.DefaultText = "";
             this.txtPass.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.txtPass.Location = new System.Drawing.Point(249, 269);
+            this.txtPass.Location = new System.Drawing.Point(240, 270);
             this.txtPass.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtPass.Name = "txtPass";
             this.txtPass.PlaceholderText = "Password";
             this.txtPass.SelectedText = "";
-            this.txtPass.Size = new System.Drawing.Size(229, 48);
-            this.txtPass.TabIndex = 1;
+            this.txtPass.Size = new System.Drawing.Size(200, 40);
+            this.txtPass.TabIndex = 2;
             // 
             // txtName
             // 
             this.txtName.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.txtName.DefaultText = "";
             this.txtName.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.txtName.Location = new System.Drawing.Point(20, 337);
+            this.txtName.Location = new System.Drawing.Point(20, 330);
             this.txtName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtName.Name = "txtName";
             this.txtName.PlaceholderText = "Họ tên";
             this.txtName.SelectedText = "";
-            this.txtName.Size = new System.Drawing.Size(229, 48);
-            this.txtName.TabIndex = 2;
+            this.txtName.Size = new System.Drawing.Size(200, 40);
+            this.txtName.TabIndex = 3;
             // 
             // txtCCCD
             // 
             this.txtCCCD.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.txtCCCD.DefaultText = "";
             this.txtCCCD.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.txtCCCD.Location = new System.Drawing.Point(275, 337);
+            this.txtCCCD.Location = new System.Drawing.Point(240, 330);
             this.txtCCCD.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtCCCD.Name = "txtCCCD";
             this.txtCCCD.PlaceholderText = "CCCD";
             this.txtCCCD.SelectedText = "";
-            this.txtCCCD.Size = new System.Drawing.Size(229, 48);
-            this.txtCCCD.TabIndex = 3;
+            this.txtCCCD.Size = new System.Drawing.Size(200, 40);
+            this.txtCCCD.TabIndex = 4;
             // 
             // txtSDT
             // 
             this.txtSDT.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.txtSDT.DefaultText = "";
             this.txtSDT.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.txtSDT.Location = new System.Drawing.Point(531, 337);
+            this.txtSDT.Location = new System.Drawing.Point(460, 330);
             this.txtSDT.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtSDT.Name = "txtSDT";
             this.txtSDT.PlaceholderText = "SĐT";
             this.txtSDT.SelectedText = "";
-            this.txtSDT.Size = new System.Drawing.Size(201, 48);
-            this.txtSDT.TabIndex = 4;
+            this.txtSDT.Size = new System.Drawing.Size(200, 40);
+            this.txtSDT.TabIndex = 5;
             // 
             // txtEmail
             // 
             this.txtEmail.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.txtEmail.DefaultText = "";
             this.txtEmail.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.txtEmail.Location = new System.Drawing.Point(762, 337);
+            this.txtEmail.Location = new System.Drawing.Point(680, 330);
             this.txtEmail.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtEmail.Name = "txtEmail";
             this.txtEmail.PlaceholderText = "Email";
             this.txtEmail.SelectedText = "";
-            this.txtEmail.Size = new System.Drawing.Size(229, 48);
-            this.txtEmail.TabIndex = 5;
+            this.txtEmail.Size = new System.Drawing.Size(200, 40);
+            this.txtEmail.TabIndex = 6;
             // 
             // cboRole
             // 
@@ -177,19 +177,19 @@ namespace AnTam_BaoHiem.Views
             this.cboRole.Items.AddRange(new object[] {
             "Admin",
             "KhachHang"});
-            this.cboRole.Location = new System.Drawing.Point(519, 281);
+            this.cboRole.Location = new System.Drawing.Point(460, 270);
             this.cboRole.Name = "cboRole";
-            this.cboRole.Size = new System.Drawing.Size(145, 36);
-            this.cboRole.TabIndex = 6;
+            this.cboRole.Size = new System.Drawing.Size(200, 36);
+            this.cboRole.TabIndex = 7;
             // 
             // btnAdd
             // 
             this.btnAdd.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.btnAdd.ForeColor = System.Drawing.Color.White;
-            this.btnAdd.Location = new System.Drawing.Point(20, 459);
+            this.btnAdd.Location = new System.Drawing.Point(20, 420);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(180, 45);
-            this.btnAdd.TabIndex = 7;
+            this.btnAdd.TabIndex = 8;
             this.btnAdd.Text = "Thêm";
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
@@ -197,10 +197,10 @@ namespace AnTam_BaoHiem.Views
             // 
             this.btnUpdate.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.btnUpdate.ForeColor = System.Drawing.Color.White;
-            this.btnUpdate.Location = new System.Drawing.Point(249, 459);
+            this.btnUpdate.Location = new System.Drawing.Point(240, 420);
             this.btnUpdate.Name = "btnUpdate";
             this.btnUpdate.Size = new System.Drawing.Size(180, 45);
-            this.btnUpdate.TabIndex = 8;
+            this.btnUpdate.TabIndex = 9;
             this.btnUpdate.Text = "Sửa";
             this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
@@ -208,16 +208,16 @@ namespace AnTam_BaoHiem.Views
             // 
             this.btnDelete.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.btnDelete.ForeColor = System.Drawing.Color.White;
-            this.btnDelete.Location = new System.Drawing.Point(484, 459);
+            this.btnDelete.Location = new System.Drawing.Point(460, 420);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(180, 45);
-            this.btnDelete.TabIndex = 9;
+            this.btnDelete.TabIndex = 10;
             this.btnDelete.Text = "Xóa";
             this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // TaiKhoanForm
             // 
-            this.ClientSize = new System.Drawing.Size(1020, 538);
+            this.ClientSize = new System.Drawing.Size(1020, 500);
             this.Controls.Add(this.dgv);
             this.Controls.Add(this.txtUser);
             this.Controls.Add(this.txtPass);
@@ -231,6 +231,7 @@ namespace AnTam_BaoHiem.Views
             this.Controls.Add(this.btnDelete);
             this.Name = "TaiKhoanForm";
             this.Text = "Quản lý tài khoản";
+            this.Load += new System.EventHandler(this.TaiKhoanForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
             this.ResumeLayout(false);
 
